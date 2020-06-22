699a006f10120249cd470fe2f93f4ec86ccf2b91
= <tramtruongrcm@gmail.com>
2020-06-22 10:41:17 +0700

    update postComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DefaultModule } from './layouts/default/default.module';
+import { PosteditComponent } from './modules/posts/postedit/postedit.component';
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    PosteditComponent,
 
   ],
   imports: [
